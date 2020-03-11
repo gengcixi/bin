77b75825355439d4ba225f60b1346ba852915530
Cixi Geng <cixi.geng1@unisoc.com>
2020-03-11 10:36:12 +0800

    rename parse ltp log

--- parse-ltp-log.py
+++ parse-ltp-log.py
@@ -4,20 +4,22 @@
 import sys
 import csv
 import io
+import os
 
 log_file = str(sys.argv[1])
-csv_file = log_file.split('.')[0]
+csv_file = log_file[::-1].split('.',1)[1][::-1] + '.csv'
 
 testNum=0
 testcase=''
 test_log=''
+test_result=''
 case_log_end = 'execution_status'
 case_log_start = 'test_output'
 
-def csvWrite(a,b,c,d):
-    with io.open(csv_file+'.csv','ab+') as csv_f:
+def csvWrite(a,b,c,d,e):
+    with io.open(csv_file,'ab+') as csv_f:
         writer = csv.writer(csv_f,dialect='excel')
-        writer.writerow([a,b,c,d])
+        writer.writerow([a,b,c,d,e])
 
 def findLog(N):
     logs = []
@@ -25,7 +27,6 @@ def findLog(N):
         for line in file.readlines()[N:]:
             if case_log_end in line :
                 break
-#            line = line.strip()
             logs.append(line)
             result = ''.join(logs)
         return result
@@ -41,15 +42,24 @@ def findResult(N):
 
 def ReadLogs():
     global testNum
-
+    global test_result
     with open(log_file, 'r') as f:
         for (num,value) in enumerate(f):
             if 'tag=' in value :
                 testNum +=1
                 testcase = value.split('tag=')[1].split(' ')[0]
                 test_log = findLog(num+4)
-                test_result = findResult(num)
-                csvWrite(testNum,testcase,test_result,test_log)
+                return_value = findResult(num)
+                if int(return_value) == 0 :
+                    test_result = 'PASS'
+                elif int(return_value) == 32 :
+                    test_result = 'CONF'
+                else :
+                    test_result = 'FAIL'
+                csvWrite(testNum,testcase,test_result,return_value,test_log)
                 
-csvWrite('No.','Testcase','return_value','Test_log')
+if os.path.exists(csv_file):
+    os.remove(csv_file)
+                 
+csvWrite('No.','Testcase','Test_result','Return_value','Test_log')
 ReadLogs()
