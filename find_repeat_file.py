89d5c019657b2b93aa4652b87e7a19334fb9327a
Cixi Geng <gengcixi@163.com>
2020-07-06 07:49:45 +0800

    update find_repeat_file

--- find_repeat_file.py
+++ find_repeat_file.py
@@ -3,7 +3,6 @@
 
 import os
 import hashlib
-import time
 import sys
 
 # get file MD5 
@@ -17,38 +16,37 @@ def get_ms5(filename):
 
 #遍历filepath下所有文件，包括子目录
 def find_all_file(filepath):
+    global filelist
     files = os.listdir(filepath)
     for fi in files:
         fi_d = os.path.join(filepath,fi)
         if os.path.isdir(fi_d):
+            try:
+                os.rmdir(fi_d)
+                print("删除空文件夹：%s" %fi_d)
+            except Exception as e:
+                print("非空文件夹：%s" %fi_d)
             find_all_file(fi_d)
         else:
             file=os.path.join(filepath,fi_d)
             filelist.append(file)
-            print(file)
-        return filelist
-filelist=find_all_file(path)
-
-# get file lists
-def get_urllist(base):
-    list = os.listdir(base)
-    urllist = []
-    for i in list:
-        url = base + i
-        urllist.append(url)
-    return urllist
+#    print(filelist)
+    return filelist
 
 # main function
-
 if __name__ == '__main__':
     md5list = []
+    filelist = []
     path = str(sys.argv[1])
-    urllist = get_urllist(path)
-    print("test")
+#    urllist = get_urllist(path)
+    urllist = find_all_file(path)
+#    print(urllist)
     for a in urllist:
         md5 = get_ms5(a)
         if (md5 in md5list):
             print("重复文件：%s" % a)
+            os.remove(a)
+            print("删除%s完成" %a)
         else:
             md5list.append(md5)
-            print("Total %s file" % len(md5list))
+ #           print("Total %s file" % len(md5list))
